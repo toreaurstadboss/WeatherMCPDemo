36631fb40c2fcf7d940195e92327d1af49e4f18f
Tore Aurstad <tore.aurstad@gmail.com>
2025-10-11 21:34:27 +0200

    Copied the code from the MCP server project to the MCP http server project. This code will be eventually moved into a Common project to avoid replicating the code.

---
+++ WeatherServer.Http/Common/WeatherServerApiClientNames.cs
@@ -0,0 +1,12 @@
+namespace WeatherServer.Common;
+
+public static class WeatherServerApiClientNames
+{
+    
+    public const string WeatherGovApiClientName = "weathergovapi-demo1";
+
+    public const string YrApiClientName = "yrapi-demo1";
+
+    public const string OpenStreetmapApiClientName = "nomainatim-openstreetmap-demo1";
+
+}
--- WeatherServer.Http/Program.cs
+++ WeatherServer.Http/Program.cs
@@ -1,3 +1,7 @@
+using System.Net.Http.Headers;
+using WeatherServer.Common;
+using WeatherServer.Tools;
+
 namespace WeatherServer.Http
 {
     public class Program
@@ -8,9 +12,49 @@ namespace WeatherServer.Http
 
             // Add services to the container.
 
+            // Add MCP support
+            builder.Services
+                .AddMcpServer()
+                .WithStdioServerTransport()
+                .WithTools<YrTools>()
+                .WithTools<UnitedStatesWeatherTools>()
+                .WithTools<NominatimTols>();
+
+            //Add swagger support
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Configure logging
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole(options =>
+            {
+                options.LogToStandardErrorThreshold = LogLevel.Warning;
+            });
+            builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
+            // Add named Http clients that fetches more data from external APIs
+
+            builder.Services.AddHttpClient(WeatherServerApiClientNames.WeatherGovApiClientName, client =>
+            {
+                client.BaseAddress = new Uri("https://api.weather.gov");
+                client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("us-weather-democlient2-tool", "1.0"));
+            });
+
+            builder.Services.AddHttpClient(WeatherServerApiClientNames.YrApiClientName, client =>
+            {
+                client.BaseAddress = new Uri("https://api.met.no");
+                client.DefaultRequestHeaders.UserAgent.Clear();
+                client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("yrweather-mcpdemoclient2-tore-tool", "1.0"));
+                //client.DefaultRequestHeaders.UserAgent.ParseAdd("ToresMcpDemo/1.0 (+https://github.com/toreaurstadboss)");
+            });
+
+            builder.Services.AddHttpClient(WeatherServerApiClientNames.OpenStreetmapApiClientName, client =>
+            {
+                client.BaseAddress = new Uri("https://nominatim.openstreetmap.org");
+                client.DefaultRequestHeaders.UserAgent.Clear();
+                client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("nominatim-openstreetmap-client2-api-tool", "1.0"));
+            });
 
             var app = builder.Build();
 
